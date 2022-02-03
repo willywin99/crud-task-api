21e60b19bd97729b49406468fc4b1c21cb36e019
willywin99 <willywinsianturi@gmail.com>
2022-02-03 11:51:35 +0700

    added update and delete route

--- app.js
+++ app.js
@@ -1,7 +1,118 @@
 const express = require('express');
 const app = express();
-
 const mongoose = require('./database/mongoose')
+
+const TaskList = require('./database/models/taskList');
+const Task = require('./database/models/task');
+
+/*
+  CORS - Cross Origin Request Security
+  Backend - http://localhost:3000
+  Frontend - http://localhost:4200
+*/
+// 3rd party library, app.use(cors());
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With, Content-Type, Accept');
+
+  // Pass to next layer of middleware
+  next();
+});
+
+// Example of middleware
+app.use(express.json());  // Or 3rd party bodyParser
+
+// Routes of REST API Endpoints or RESTful webservices Endpoints
+/*
+  TaskList - Create, Update, ReadTaskListById, ReadAllTaskList
+  Task - Create, Update, ReadTaskById, ReadAllTask
+*/
+
+// Routes or API endpoints for TaskList model
+// Get all Task Lists
+// http://localhost:3000/tasklists => [ {TaskList}, {TaskList} ]
+app.get('/tasklists', (req, res) => {
+  TaskList.find({})
+    .then((lists) => { 
+      res.status(200);
+      res.send(lists);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500);
+    });
+});
+
+// Route or Endpoint for Creating a TaskList
+app.post('/tasklists', (req, res) => {
+  // console.log("Hello i am inside post method");
+  console.log(req.body);
+
+  let taskListObj = { 'title': req.body.title };
+  TaskList(taskListObj).save()
+    .then((taskList) => { 
+      res.status(201);
+      res.send(taskList);
+    })
+    .catch((error) => { 
+      console.log(error);
+      res.status(500);
+    });
+});
+
+// Endpoint to get one tasklist by taskListId: http://localhost:3000/tasklists/61fae4597db5e686809a1db4
+app.get('/tasklists/:taskListId', (req, res) => {
+  let taskListId = req.params.taskListId;
+  TaskList.find({ _id: taskListId })
+    .then((taskList) => {
+      res.status(200).send(taskList)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// PUT is full update of object
+app.put('/tasklists/:taskListId', (req, res) => {
+  TaskList.findOneAndUpdate({ _id: req.params.taskListId }, { $set: req.body })
+    .then((taskList) => {
+      res.status(200).send(taskList)
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500);
+    });
+});
+// PATCH is partial update of one field of an object
+app.patch('/tasklists/:taskListId', (req, res) => {
+  TaskList.findOneAndUpdate({ _id: req.params.taskListId }, { $set: req.body })
+    .then((taskList) => {
+      res.status(200).send(taskList)
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500);
+    });
+});
+
+// DELETE a tasklist by id
+app.delete('/tasklists/:taskListId', (req, res) => {
+  TaskList.findByIdAndDelete(req.params.taskListId)
+    .then((taskList) => {
+      res.status(201).send(taskList)
+    })
+    .catch(
+      (error) => { console.log(error) }
+    )
+});
 
 // app.listen(3000, function() {
 //   console.log("Server started on port 3000");
